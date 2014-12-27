03c10da06739984d713c5e78bc4267f3ca854cbe
Ivaylo Hristov <var.ivaylohristov@gmail.com>
2014-12-27 22:25:25 +0200

    Winter HW - Task 03

--- HW06_JQuery_Ivaylo_Hristov_13/hw.js
+++ HW06_JQuery_Ivaylo_Hristov_13/hw.js
@@ -1,4 +1,9 @@
 $(function() {
+	'use strict';
+	
 	// Task 02
 	console.log($('div#footer a:first').attr('title'));
+
+	// Task 03
+	console.log($('div.inscreen div:first').children('p').text());
 });
