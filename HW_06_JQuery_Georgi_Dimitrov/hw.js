9202384d6e043731c4516d85273d6497813ac029
georgidimitrov <georgu.dimitrov@gmail.com>
2014-12-27 22:39:25 +0200

    Task 05

--- HW_06_JQuery_Georgi_Dimitrov/hw.js
+++ HW_06_JQuery_Georgi_Dimitrov/hw.js
@@ -7,6 +7,9 @@ $(function() {
 	console.log($('div.inscreen div#col1').children('p').text());
 
 	//Task 04
-	$('ul#menu-top-level-menu').append('<li id="new-button"><a href="#"> New Button </a></li>')
+	$('ul#menu-top-level-menu').append('<li id="new-button"><a href="#"> New Button </a></li>');
+
+	//Task 05
+	$('div#footer').prepend('<div id="dynamiccontent"></div>');
 
 })
