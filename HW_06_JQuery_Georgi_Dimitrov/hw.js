b1591b0d89d07796c2625c7d46090e4007a03fac
georgidimitrov <georgu.dimitrov@gmail.com>
2014-12-27 22:34:34 +0200

    Task 04

--- HW_06_JQuery_Georgi_Dimitrov/hw.js
+++ HW_06_JQuery_Georgi_Dimitrov/hw.js
@@ -6,6 +6,7 @@ $(function() {
 	//Task 03
 	console.log($('div.inscreen div#col1').children('p').text());
 
-
+	//Task 04
+	$('ul#menu-top-level-menu').append('<li id="new-button"><a href="#"> New Button </a></li>')
 
 })
