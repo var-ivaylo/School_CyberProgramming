f134739314917dbd7db76a08952c2fd254f4d123
Ivaylo Hristov <var.ivaylohristov@gmail.com>
2014-12-27 22:34:25 +0200

    Winter HW - Task 04

--- HW06_JQuery_Ivaylo_Hristov_13/hw.js
+++ HW06_JQuery_Ivaylo_Hristov_13/hw.js
@@ -1,9 +1,12 @@
 $(function() {
 	'use strict';
-	
+
 	// Task 02
 	console.log($('div#footer a:first').attr('title'));
 
 	// Task 03
 	console.log($('div.inscreen div:first').children('p').text());
+
+	// Task 04
+	$('ul#menu-top-level-menu').append('<li id="menu-item-new-bttn"><a href="#">new button</a></li>');
 });
